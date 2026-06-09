e638e0d363864ff5f1f4123be08ea69ffa6284b2
omidsdgi <omid69sdgi@gmail.com>
2026-06-09 20:43:18 +0330

    feat: add useDarkMode hook

--- src/components/context/ThemeContext.tsx
+++ src/components/context/ThemeContext.tsx
@@ -1,9 +1,22 @@
-const ThemeContext = () => {
+import {useEffect, useState} from "react";
+
+const useDarkMode = () => {
+    const[isDark, setIsDark] = useState(()=>{
+        if (typeof window !== "undefined") return false;
+        return localStorage.getItem('theme')=== 'dark';
+    })
+
+    useEffect(() => {
+        document.documentElement.classList.toggle('dark',isDark);
+        localStorage.setItem('theme',isDark ? 'dark' : 'light');
+    }, [isDark]);
+    
+    const toggleTheme = () => {
+        setIsDark(prevState => !prevState);
+    }
     return (
-        <div>
-            ThemeContext
-        </div>
+        {isDark ,toggleTheme}
     );
 };
 
-export default ThemeContext;
+export default useDarkMode;
