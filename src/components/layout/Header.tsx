e95b9bd01cfc3093f3474efbc9ab8162766542b3
omidsdgi <omid69sdgi@gmail.com>
2026-06-09 19:40:37 +0330

    feat: implement Header component with login form

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 const Header = () => {
     return (
-        <nav>
-            <p>Log in to get started</p>
-            <Image src='/logo.png' alt={'Bankist logo'} width={100} height={100} />
-            <form>
-            <input type='text' placeholder='user' />
-            <input type="password" placeholder='PIN' maxLength={4} />
-                <button>&rarr;</button>
+        <nav className="flex items-center justify-between py-6 ">
+
+            <p className='text-xl font-light'>Log in to get started</p>
+            <Image src='/logo.png' alt={'Bankist logo'} width={52.5} height={52.5}/>
+            <form className='flex items-center justify-center gap-3'>
+                <input type='text' placeholder='user' className='login--input '/>
+                <input type="password" placeholder='PIN' maxLength={4} className='login--input '/>
+                <button className='border-none bg-transparent text-xl  cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-500 focus:outline-none focus:text-gray-500'> &rarr;</button>
             </form>
         </nav>
     );
