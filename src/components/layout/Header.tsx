84b09f90276db0e6d85c7d8934d62a4b9cba2fc0
omidsdgi <omid69sdgi@gmail.com>
2026-06-09 12:24:43 +0330

    chore: create initial Header component structure

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <nav>
+            <p>Log in to get started</p>
+            <Image src='/logo.png' alt={'Bankist logo'} width={100} height={100} />
+            <form>
+            <input type='text' placeholder='user' />
+            <input type="password" placeholder='PIN' maxLength={4} />
+                <button>&rarr;</button>
+            </form>
+        </nav>
     );
 };
 
