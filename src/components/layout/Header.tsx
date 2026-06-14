97f34771c6f0e14783ef12b55fb094116b6eb2c2
omidsdgi <omid69sdgi@gmail.com>
2026-06-14 11:06:33 +0330

    style: adjust header style

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,14 +3,14 @@ import {ThemeToggle} from "@/components";
 
 const Header = () => {
     return (
-        <nav className="flex items-center justify-between py-6 ">
+        <nav className="flex flex-wrap gap4 items-center justify-between px-4 sm:px-8 py-6">
 
-            <p className='text-xl font-medium'>Log in to get started</p>
-            <Image src='/logo.png' alt={'Bankist logo'} width={52.5} height={52.5}/>
-            <form className='flex items-center gap-4'>
+            <p className='text-sm md:text-lg lg:text-2xl font-medium'>Log in to get started</p>
+            <Image src='/logo.png' alt="Bankist logo" width={52.5} height={52.5}/>
+            <form className='flex items-center gap-4 flex-wrap'>
                 <input type='text' placeholder='user' className='login--input '/>
                 <input type="password" placeholder='PIN' maxLength={4} className='login--input '/>
-                <button className='border-none bg-transparent text-xl  mr-3 cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-500 focus:outline-none focus:text-gray-500'> &rarr;</button>
+                <button className='border-none bg-transparent text-2xl cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-500 focus:outline-none focus:text-gray-500'>&rarr;</button>
             </form>
                 <ThemeToggle />
         </nav>
