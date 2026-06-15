74ec0fafc639904fd822378c735f4f9503c28f97
omidsdgi <omid69sdgi@gmail.com>
2026-06-15 18:59:32 +0330

    refactor: improve header layout and add IconButton component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,26 @@
 import Image from "next/image";
-import {ThemeToggle} from "@/components";
+import {IconButton, ThemeToggle} from "@/components";
 
 const Header = () => {
     return (
-        <nav className="flex flex-wrap gap4 items-center justify-between px-4 sm:px-8 py-6">
+        <header className="flex flex-col-2 gap-4 flex-wrap items-center justify-between md:flex-row  px-4 md:px-8 py-6">
 
-            <p className='text-sm md:text-lg lg:text-2xl font-medium'>Log in to get started</p>
+            <h1 className='text-2xl font-medium'>Log in to get started</h1>
             <Image src='/logo.png' alt="Bankist logo" width={52.5} height={52.5}/>
-            <form className='flex items-center gap-4 flex-wrap'>
-                <input type='text' placeholder='user' className='login--input '/>
-                <input type="password" placeholder='PIN' maxLength={4} className='login--input '/>
-                <button className='border-none bg-transparent text-2xl cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-500 focus:outline-none focus:text-gray-500'>&rarr;</button>
+            <form className='flex items-center gap-4'>
+                <input
+                    type='text'
+                    placeholder='user'
+                    className='login--input '/>
+                <input
+                    type="password"
+                    placeholder='PIN'
+                    maxLength={4}
+                    className='login--input '/>
+                <IconButton/>
             </form>
                 <ThemeToggle />
-        </nav>
+        </header>
     );
 };
 
