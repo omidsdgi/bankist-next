2897bf44eb913eda8c520475bbbb924516746d64
omidsdgi <omid69sdgi@gmail.com>
2026-06-15 19:16:43 +0330

    style: adjust Balance component style

--- src/components/sections/Balance.tsx
+++ src/components/sections/Balance.tsx
@@ -1,8 +1,8 @@
 const Balance = () => {
     return (
-        <section className="flex items-end justify-between flex-wrap gap-4 mb-2 col-span-2 ">
+        <section className="flex items-end justify-between gap-4 ">
             <div className='flex flex-col'>
-                <p className='text-xl font-medium '>Current balance</p>
+                <h2 className='text-2xl font-medium '>Current balance</h2>
                 <p className='text-sm '>As of <span>05/03/02037</span></p>
             </div>
             <p className='text-4xl sm:text-5xl font-normal'>446100€</p>
