25a0d3cda20e6f369aee81b4020b1360d955dd19
omidsdgi <omid69sdgi@gmail.com>
2026-06-15 09:24:50 +0330

    style: adjust balance style

--- src/components/sections/Balance.tsx
+++ src/components/sections/Balance.tsx
@@ -1,12 +1,12 @@
 const Balance = () => {
     return (
-        <div>
-            <div>
-                <p>Current balance</p>
-                <p>As of <span>05/03/02037</span></p>
+        <section className="flex items-end justify-between flex-wrap gap-4 mb-2 col-span-2 ">
+            <div className='flex flex-col'>
+                <p className='text-xl font-medium '>Current balance</p>
+                <p className='text-sm '>As of <span>05/03/02037</span></p>
             </div>
-            <p>0000€</p>
-        </div>
+            <p className='text-4xl sm:text-5xl font-normal'>446100€</p>
+        </section>
     );
 };
 
