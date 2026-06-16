fa8008b15125c60584d3f555e23e797dd6cdf3bb
omidsdgi <omid69sdgi@gmail.com>
2026-06-16 11:46:58 +0330

    feat: add static name to MovementItem component style: adjust MovementItem styling

--- src/components/sections/MovementItem.tsx
+++ src/components/sections/MovementItem.tsx
@@ -1,7 +1,7 @@
-const MovementItem = ({account, key}) => {
+const MovementItem = () => {
     return (
-        <li className='flex items-center justify-between gap-4 px-4 sm:px-8 py-4 border-b border-gray-100'>
-            <span className='text-xs uppercase font-medium text-white px-3 py-1 rounded-full bg-gradient-to-tl from-emerald-500 to-lime-400 '>{account.owner}</span>
+        <li className='flex items-center justify-between gap-4  py-4 border-b border-gray-100'>
+            <span className='text-xs uppercase font-medium text-white px-3 py-1 rounded-full bg-gradient-to-tl from-emerald-500 to-lime-400 '>Omid</span>
             <span className='text-xs uppercase font-medium text-gray-500'>2037/05/15</span>
             <span className='text-lg ml-auto'>1300€</span>
         </li>
