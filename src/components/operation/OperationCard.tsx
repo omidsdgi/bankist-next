0ad2b92be6675a40082c5ccca57bbbd6783529b2
omidsdgi <omid69sdgi@gmail.com>
2026-06-16 14:14:15 +0330

    style: adjust OperationCard style

--- src/components/operation/OperationCard.tsx
+++ src/components/operation/OperationCard.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 
 interface OperationCardProps {
-    title?: string;
+    title: string;
     variant:  'transfer' | 'loan' | 'close';
     children: React.ReactNode;
 }
 
 const variantStyles = {
-transfer: 'bg-gradient-to-tl from-amber-400 to-yellow-300 ',
+transfer: 'bg-gradient-to-tl from-amber-500 to-yellow-400 ',
     loan: 'bg-gradient-to-tl from-emerald-500 to-lime-400 ',
     close: 'bg-gradient-to-tl from-rose-500 to-red-400 ',
 }
 const OperationCard = ({title, variant, children}:OperationCardProps) => {
     return (
-        <section className={`rounded-2xl p-6 sm:p-8 ${variantStyles[variant]}`}>
+        <section className={`rounded-2xl p-4 sm:p-6 ${variantStyles[variant]}`}>
             <h2 className='text-lg font-semibold mb-4'>{title}</h2>
             {children}
-            OperationCard
         </section>
     );
 };
