07824332e83bd29001ee686f7cf1932d7f6df68c
omidsdgi <omid69sdgi@gmail.com>
2026-06-16 12:50:10 +0330

    style: adjust movementItem style

--- src/components/sections/MovementItem.tsx
+++ src/components/sections/MovementItem.tsx
@@ -1,6 +1,6 @@
 const MovementItem = () => {
     return (
-        <li className='flex items-center justify-between gap-4  py-4 border-b border-gray-100'>
+        <li className='flex items-center justify-between gap-4 px-8 py-4 border-b border-gray-100'>
             <span className='text-xs uppercase font-medium text-white px-3 py-1 rounded-full bg-gradient-to-tl from-emerald-500 to-lime-400 '>Omid</span>
             <span className='text-xs uppercase font-medium text-gray-500'>2037/05/15</span>
             <span className='text-lg ml-auto'>1300€</span>
