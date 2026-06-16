a3fcb1b55bb7af17e29afe2a6b9e25a6c19d0123
omidsdgi <omid69sdgi@gmail.com>
2026-06-16 14:18:53 +0330

    style: adjust IconButton style

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -8,7 +8,7 @@ const IconButton = ({type='submit'}:ButtonProps) => {
     return (
         <button
             type={type}
-            className='text-2xl border-none bg-transparent cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-700 focus:outline-none focus:text-gray-700'>
+            className='text-xl border-none bg-white rounded-lg  cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-700 focus:outline-none focus:text-gray-700'>
             &rarr;
         </button>
     );
