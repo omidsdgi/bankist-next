70afc6a791ed8f4ad4ad960c5313d0d7f6dc9975
omidsdgi <omid69sdgi@gmail.com>
2026-06-16 14:16:38 +0330

    chore: add Typescript type to TextInput component

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,6 +1,15 @@
-const TextInput = ({className=""}:{className:string}) => {
+interface InputProps {
+    className?: string;
+    type?: string;
+    placeholder?: string;
+}
+
+const TextInput = ({className="", type='text', placeholder}:InputProps) => {
     return (
-        <input className={`w-full text-sm text-center rounded-lg px-4 py-2 outline-none focus:border-gray-300 transition-all ${className}`}/>
+        <input
+            type={type}
+            placeholder={placeholder}
+            className={`w-full text-sm text-center rounded-lg px-4 py-1 outline-none focus:border-gray-300 transition-all ${className}`}/>
             );};
 
 export default TextInput;
