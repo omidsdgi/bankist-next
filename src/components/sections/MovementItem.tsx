db78e5b8daeac94d5e124e4ad22e1f000f746995
omidsdgi <omid69sdgi@gmail.com>
2026-06-17 20:49:29 +0330

    style(MovementItem): adjust dark mode color

--- src/components/sections/MovementItem.tsx
+++ src/components/sections/MovementItem.tsx
@@ -3,7 +3,7 @@ const MovementItem = () => {
         <li className='flex items-center justify-between gap-4 px-8 py-4 border-b border-gray-100'>
             <span className='text-xs uppercase font-medium text-white px-3 py-1 rounded-full bg-gradient-to-tl from-emerald-500 to-lime-400 '>Omid</span>
             <span className='text-xs uppercase font-medium text-gray-500'>2037/05/15</span>
-            <span className='text-lg ml-auto'>1300€</span>
+            <span className='text-lg ml-auto dark:text-black'>1300€</span>
         </li>
     );
 };
