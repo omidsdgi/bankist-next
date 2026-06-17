e1c43a15cbc342cf196e4f5ff597aaf47ba2a171
omidsdgi <omid69sdgi@gmail.com>
2026-06-17 20:51:40 +0330

    style: adjust TextInput style style(IconButton): adjust dark mod text color

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -8,7 +8,7 @@ const IconButton = ({type='submit'}:ButtonProps) => {
     return (
         <button
             type={type}
-            className='text-xl border-none bg-white rounded-lg  cursor-pointer transition-all duration-300 hover:outline-none hover:text-gray-700 focus:outline-none focus:text-gray-700'>
+            className='text-xl border-none bg-white rounded-lg  cursor-pointer transition-all duration-300 dark:text-gray-500 hover:outline-none hover:text-gray-700 focus:outline-none focus:text-gray-700'>
             &rarr;
         </button>
     );
