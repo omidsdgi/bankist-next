9463bb9722aaad13320e1d0296c5c36370521fed
omidsdgi <omid69sdgi@gmail.com>
2026-06-18 10:28:04 +0330

    feat: implement login form state and handler

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,27 +1,46 @@
 import Image from "next/image";
+import React, {useState} from "react";
+import {accounts} from "@/data/mockData";
 import {IconButton, ThemeToggle} from "@/components";
+import { HeaderProps} from "@/types/Type";
 
-const Header = () => {
-    return (
-        <header className="flex flex-col-2 gap-4 flex-wrap items-center justify-between md:flex-row  px-4  py-1">
+const Header = ({setCurrentAccount}:HeaderProps) => {
+    const [userName, setUserName]=useState<string>('')
+    const [pin, setPin]=useState<string>("")
 
-            <h1 className='text-xl font-medium'>Log in to get started</h1>
-            <Image src='/logo.png' alt="Bankist logo" width={52.5} height={52.5}/>
-            <form className='grid grid-cols-[2fr_2fr_1fr] gap-4'>
-                <input
-                    type='text'
-                    placeholder='user'
-                    className='login--input '/>
-                <input
-                    type="password"
-                    placeholder='PIN'
-                    maxLength={4}
-                    className='login--input '/>
-                <IconButton/>
-            </form>
+    const handleLogin=(e:React.MouseEvent)=> {
+        e.preventDefault()
+        const account = accounts.find(
+            (acc) => acc.owner === userName && acc.pin === Number(pin)
+        )
+        if (account) {
+            setCurrentAccount(account)
+        }
+    }
+        return (
+            <header className="flex flex-col-2 gap-4 flex-wrap items-center justify-between md:flex-row  px-4  py-1">
+
+                <h1 className='text-xl font-medium'>Log in to get started</h1>
+                <Image src='/logo.png' alt="Bankist logo" width={52.5} height={52.5}/>
+                <form className='grid grid-cols-[2fr_2fr_1fr] gap-4'>
+                    <input
+                        type='text'
+                        placeholder='user'
+                        value={userName}
+                        onChange={(e) =>setUserName(e.target.value) }
+                        className='login--input '/>
+                    <input
+                        type="password"
+                        placeholder='PIN'
+                        value={pin}
+                        onChange={(e)=>setPin(e.target.value)}
+                        maxLength={4}
+                        className='login--input '/>
+                    <IconButton onClick={handleLogin}/>
+                </form>
                 <ThemeToggle />
-        </header>
-    );
-};
+            </header>
+        );
+    };
 
-export default Header;
+    export default Header;
