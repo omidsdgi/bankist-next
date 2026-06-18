2bc1c952b2edfb113a086789d84287f91b0348a0
omidsdgi <omid69sdgi@gmail.com>
2026-06-18 10:20:34 +0330

    fix: add currentAccount state with Account | null type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import {Header, MovementList, OperationPanel, Summary,} from "@/components";
 import Balance from "@/components/sections/Balance";
+import {useState} from "react";
+import {Account} from "@/types/Type";
 
 
 export default function Home() {
+    const[currentAccount, setCurrentAccount]=useState<Account|null>(null)
     return (
         <>
-            <Header/>
+            <Header setCurrentAccount={setCurrentAccount}/>
             <main className= "max-w-5xl mx-auto mt-12 opacity-100 ">
+                {currentAccount &&(
+                    <>
                 <Balance/>
                 <div className=' grid grid-cols-1  sm:grid-cols-[4fr_3fr] gap-6 mt-8'>
                 <MovementList />
                 <OperationPanel/>
                 </div>
             <Summary/>
+                    </>
+                    )}
             </main>
         </>
     )
