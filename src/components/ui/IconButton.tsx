d66e077731aa09da44eb6ff4590f456dad7a6fb2
omidsdgi <omid69sdgi@gmail.com>
2026-06-18 10:06:20 +0330

    fix(IconButton): add onClick prop and typescript type

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 interface ButtonProps {
     type?: "submit" | "button";
+    onClick?: (e:React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const IconButton = ({type='submit'}:ButtonProps) => {
+const IconButton = ({type='submit', onClick}:ButtonProps) => {
     return (
         <button
             type={type}
+            onClick={onClick}
             className='text-xl border-none bg-white rounded-lg  cursor-pointer transition-all duration-300 dark:text-gray-500 hover:outline-none hover:text-gray-700 focus:outline-none focus:text-gray-700'>
             &rarr;
         </button>
