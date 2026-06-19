bd00660983b4188a6ce9722d29f5dec98ee561a1
omidsdgi <omid69sdgi@gmail.com>
2026-06-19 20:26:58 +0330

    feat: add conditionl UI for deposit and withdrawl in movementList

--- src/components/sections/MovementItem.tsx
+++ src/components/sections/MovementItem.tsx
@@ -7,7 +7,12 @@ interface MovementItemProps {
 const MovementItem = ({amount, date}:MovementItemProps) => {
     return (
         <li className='flex items-center justify-between gap-4 px-8 py-4 border-b border-gray-100'>
-            <span className='text-xs uppercase font-medium text-white px-3 py-1 rounded-full bg-gradient-to-tl from-emerald-500 to-lime-400 '>Omid</span>
+            <span
+                className={`text-xs uppercase font-medium text-white px-3 py-1 rounded-full
+             ${amount>0 ? "bg-gradient-to-tl from-emerald-500 to-lime-400" :"bg-gradient-to-tl from-rose-500 to-red-400 "} `}
+            >
+                {amount>0 ? 'deposit' : "withdrawal"}
+            </span>
             <span className='text-xs uppercase font-medium text-gray-500'>{date}</span>
             <span className='text-lg ml-auto dark:text-black'>{amount}</span>
         </li>
