807a51b84a525133c174b8b4f58e47596436d709
omidsdgi <omid69sdgi@gmail.com>
2026-06-20 09:52:46 +0330

    feat:add GetCurrentAccount and createUsername helper for login

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import React, {useState} from "react";
-import {createUsernames} from "@/utility/createUsernames";
 import {IconButton, ThemeToggle} from "@/components";
 import { HeaderProps} from "@/types/Type";
+import {getCurrentAccount} from "@/utility/accounts/GetCurrentAccount";
+import {createUsernames} from "@/utility/accounts/CreateUsernames";
+
 
 const Header = ({currentAccount, setCurrentAccount}:HeaderProps) => {
     const [userName, setUserName]=useState<string>('')
@@ -10,41 +12,42 @@ const Header = ({currentAccount, setCurrentAccount}:HeaderProps) => {
 
     const handleLogin=(e:React.MouseEvent)=> {
         e.preventDefault()
-        const account = createUsernames.find(
-            (acc) => acc.username === userName && acc.pin === Number(pin)
-        )
-        if (account) {
-            setCurrentAccount(account)
+        const account= getCurrentAccount(createUsernames, userName)
+
+        if (account && account.pin === Number(pin)){
+            setCurrentAccount(account);
+        setUserName('');
+        setPin('')
         }
     }
-        return (
-            <header className="flex flex-col-2 gap-4 flex-wrap items-center justify-between md:flex-row  px-4  py-1">
+    return (
+        <header className="flex flex-col-2 gap-4 flex-wrap items-center justify-between md:flex-row  px-4  py-1">
 
-                <h1 className='text-xl font-medium'>
-                    {currentAccount
+            <h1 className='text-xl font-medium'>
+                {currentAccount
                     ? `Welcome back, ${currentAccount?.owner.split(" ")[0]}`
                     : "Log in to get started"}
-                </h1>
-                <Image src='/logo.png' alt="Bankist logo" width={52.5} height={52.5}/>
-                <form className='grid grid-cols-[2fr_2fr_1fr] gap-4'>
-                    <input
-                        type='text'
-                        placeholder='user'
-                        value={userName}
-                        onChange={(e) =>setUserName(e.target.value) }
-                        className='login--input '/>
-                    <input
-                        type="password"
-                        placeholder='PIN'
-                        value={pin}
-                        onChange={(e)=>setPin(e.target.value)}
-                        maxLength={4}
-                        className='login--input '/>
-                    <IconButton onClick={handleLogin}/>
-                </form>
-                <ThemeToggle />
-            </header>
-        );
-    };
+            </h1>
+            <Image src='/logo.png' alt="Bankist logo" width={52.5} height={52.5}/>
+            <form className='grid grid-cols-[2fr_2fr_1fr] gap-4'>
+                <input
+                    type='text'
+                    placeholder='user'
+                    value={userName}
+                    onChange={(e) =>setUserName(e.target.value) }
+                    className='login--input '/>
+                <input
+                    type="password"
+                    placeholder='PIN'
+                    value={pin}
+                    onChange={(e)=>setPin(e.target.value)}
+                    maxLength={4}
+                    className='login--input '/>
+                <IconButton onClick={handleLogin}/>
+            </form>
+            <ThemeToggle />
+        </header>
+    );
+};
 
-    export default Header;
+export default Header;
