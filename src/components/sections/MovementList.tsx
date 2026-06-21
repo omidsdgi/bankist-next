2557c6e4d6e841bd87c5c4e53cb1462a7b06e08f
omidsdgi <omid69sdgi@gmail.com>
2026-06-21 18:56:11 +0330

    fix: update MovementList Type

--- src/components/sections/MovementList.tsx
+++ src/components/sections/MovementList.tsx
@@ -1,11 +1,16 @@
 import MovementItem from "./MovementItem";
 import {Account} from "@/types/Type";
 
-const MovementList=({account}: {account: Account })=>{
+interface MovementListProps {
+    move: number[] | undefined
+    account:Account
+}
+
+const MovementList=({move, account}: MovementListProps)=>{
     return (
         <section className="max-h-[500px] overflow-y-auto bg-white rounded-2xl shadow">
             <ul className='divide-y divide-gray-500'>
-                {account.movements.map((movement, index) => {
+                {move?.map((movement, index) => {
                     return <MovementItem
                         key={index}
                         amount={movement}
