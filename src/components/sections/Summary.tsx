0485e291197610fa45c88537a5421aa0ed0dbd03
omidsdgi <omid69sdgi@gmail.com>
2026-06-23 13:08:33 +0330

    refactor(Summary):consume totalIn, totalOut, interest directly from useAccountSummary

--- src/components/sections/Summary.tsx
+++ src/components/sections/Summary.tsx
@@ -1,58 +1,36 @@
 import {Account} from "@/types/Type";
+import {useAccountSummary} from "@/hook/UseAccountSummary";
 
 interface SummaryProps {
     isSorted:boolean;
     setIsSorted:(isSorted:boolean) => void;
     account:Account
 }
-    const formatCur=function (value:number, locale:string, currency:string){
-        return new Intl.NumberFormat(locale, {
-            style: "currency",
-            currency: currency
-        }).format(value);
-    }
 
 const Summary = ({isSorted, setIsSorted,account}:SummaryProps) => {
+    const {totalIn, totalOut, interest}=useAccountSummary(account)
 
-    const totalIn=account.movements
-        .filter(movement => movement>0)
-        .reduce((acc, mov) => acc + mov, 0)
-        .toFixed(2);
-
-    const totalOut=account.movements
-        .filter(movement => movement<0)
-        .reduce((acc, mov) => acc + mov, 0)
-        .toFixed(2)
-
-
-    const interest=account.movements
-        .filter(move => move>0)
-            .map(deposit =>(deposit* account.interestRate)/100)
-            .filter(int=>int>1)
-            .reduce((acc, int) => acc + int, 0)
-        .toFixed(2)
-
-    return (
+        return (
         <footer className="grid grid-cols-1  sm:grid-cols-[4fr_3fr] gap-6 mt-8 text-xs md:text-sm text-gray-600 dark:text-gray-400">
             <div className="flex items-center  justify-between flex-1 ">
                 <div>
                     <span className="mr-2 uppercase">In</span>
                     <span className="font-medium text-green-500">
-    {formatCur(+totalIn, account.locale, account.currency)}
+    {totalIn}
       </span>
                 </div>
 
                 <div>
                     <span className="mr-2 uppercase">Out</span>
                     <span className="font-medium text-red-500">
-        {formatCur(Math.abs(+totalOut), account.locale, account.currency)}
+        {totalOut}
       </span>
                 </div>
 
                 <div>
                     <span className="mr-2 uppercase">Interest</span>
                     <span className="font-medium text-green-500">
-    {formatCur(+interest, account.locale, account.currency)}
+{interest}
       </span>
                 </div>
 
