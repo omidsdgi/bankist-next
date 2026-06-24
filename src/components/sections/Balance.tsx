44b2ee6884e379b5455914cd513f60071057e87d
omidsdgi <omid69sdgi@gmail.com>
2026-06-24 10:01:47 +0330

    feat(balance): display current date using formatDateTime

--- src/components/sections/Balance.tsx
+++ src/components/sections/Balance.tsx
@@ -1,5 +1,6 @@
 import {formatCur} from "@/hook/UseAccountSummary";
 import {Account} from "@/types/Type";
+import {formatDateTime} from "@/hook/UseDateCalculation";
 
 const Balance = ({account}: { account:Account })  => {
     const balance=account.movements.reduce((acc,mov)=> acc + mov ,0)
@@ -7,7 +8,7 @@ const Balance = ({account}: { account:Account })  => {
         <section className="flex items-end justify-between gap-4 ">
             <div className='flex flex-col'>
                 <h2 className='text-2xl font-medium '>Current balance</h2>
-                <p className='text-sm '>As of <span>05/03/02037</span></p>
+                <p className='text-sm '>As of <span>{formatDateTime(account)}</span></p>
             </div>
             <p className='text-4xl sm:text-5xl font-normal'>{formatCur(balance, account.locale,account.currency )}</p>
         </section>
