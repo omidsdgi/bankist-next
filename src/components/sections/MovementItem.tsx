1d162f0c226c5a1b8f9555febe4976ea08369cd7
omidsdgi <omid69sdgi@gmail.com>
2026-06-25 09:11:11 +0330

    fix: restyle MovementItem  to align items

--- src/components/sections/MovementItem.tsx
+++ src/components/sections/MovementItem.tsx
@@ -11,14 +11,16 @@ interface MovementItemProps {
 
 const MovementItem = ({amount, date, locale, currency}:MovementItemProps) => {
     return (
-        <li className='flex items-center justify-between gap-4 px-8 py-4 border-b border-gray-100 even:bg-gray-300'>
+        <li className='grid grid-cols-[3fr_4fr] px-8 py-4 border-b border-gray-100 even:bg-gray-300'>
+            <div className='flex items-start gap-6'>
             <span
-                className={`text-xs uppercase font-medium text-white px-3 py-1 rounded-full
-             ${amount>0 ? "bg-gradient-to-tl from-emerald-500 to-lime-400" :"bg-gradient-to-tl from-rose-500 to-red-400 "} `}
+                className={`text-xs uppercase font-medium text-white px-4 py-1 rounded-full
+             ${amount>0 ? "bg-gradient-to-tl from-emerald-500 to-lime-400 px-8" :"bg-gradient-to-tl from-rose-500 to-red-400 "} `}
             >
                 {amount>0 ? 'deposit' : "withdrawal"}
             </span>
             <span className='text-xs uppercase font-medium text-gray-500'>{formatMovementDate(date)}</span>
+            </div>
             <span className='text-lg ml-auto dark:text-black'>{formatCur(amount,locale, currency)}</span>
         </li>
     );
