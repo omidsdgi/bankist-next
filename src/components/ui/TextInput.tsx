aac40730b20e7dfdb9e02b744fd7f54ab922b1cf
omidsdgi <omid69sdgi@gmail.com>
2026-06-26 13:51:58 +0330

    feat: add missing value and onChange props to input element

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,14 +1,20 @@
+import React from "react";
+
 interface InputProps {
     className?: string;
     type?: string;
     placeholder?: string;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextInput = ({className="", type='text', placeholder}:InputProps) => {
+const TextInput = ({className="", type='text', placeholder,value, onChange}:InputProps) => {
     return (
         <input
             type={type}
             placeholder={placeholder}
+            value={value}
+            onChange={onChange}
             className={`w-full text-xl text-center rounded-lg px-4 py-1 outline-none focus:border-gray-300 transition-all ${className}`}/>
             );};
 
