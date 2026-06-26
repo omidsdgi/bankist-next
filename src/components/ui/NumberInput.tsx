0fa1f9ed08eead419b8a10f515ca8048fbcc9f53
omidsdgi <omid69sdgi@gmail.com>
2026-06-26 13:50:51 +0330

    fix: add missing value and onChange props to input element

--- src/components/ui/NumberInput.tsx
+++ src/components/ui/NumberInput.tsx
@@ -4,13 +4,17 @@ interface NumberProps {
     type?: number ;
     placeholder?: string ;
     className?: string ;
+    value?: string ;
+    onChange?: (e:React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const NumberInput = ({className="", placeholder}:NumberProps) => {
+const NumberInput = ({className="", placeholder, value, onChange}:NumberProps) => {
     return (
         <input
             type='number'
             placeholder={placeholder}
+            value={value}
+            onChange={onChange}
             className={`w-full text-xl text-center rounded-lg px-4 py-1 outline-none focus:border-gray-300 transition-all ${className}`}   />
     );
 };
