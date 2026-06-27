1677e0a59df84a84bf38e61221d5b37325746e83
omidsdgi <omid69sdgi@gmail.com>
2026-06-27 17:57:36 +0330

    refactor: convert creatUsername from static array to pure function

--- src/utility/accounts/CreateUsernames.ts
+++ src/utility/accounts/CreateUsernames.ts
@@ -1,10 +1,12 @@
-import {initialAccounts} from "@/data/mockData";
+import {Account} from "@/types/Type";
 
-export const createUsernames = initialAccounts.map(acc => ({
-    ...acc,
-        username:acc.owner
+export const createUsernames = (accounts:Account[]):Account[] => {
+    return accounts.map((acc) => ({
+        ...acc,
+        username: acc.owner
             .toLowerCase()
             .split(" ")
-            .map((name)=>name[0])
+            .map((name) => name[0])
             .join("")
-}))
+    }))
+}
