e53e89a09499c3d96a16eaabfaba114ffcd46b31
omidsdgi <omid69sdgi@gmail.com>
2026-06-28 22:12:25 +0330

    feat: add update logic to transferForm

--- src/components/operation/TransferForm.tsx
+++ src/components/operation/TransferForm.tsx
@@ -1,22 +1,56 @@
 import {IconButton, NumberInput, OperationCard, TextInput} from "@/components";
-import React from "react";
+import React, {useState} from "react";
 import {getCurrentAccount} from "@/utility/accounts/GetCurrentAccount";
 import {TransferFormData} from "@/types/Type";
 
-const TransferForm = ({accounts,setAccounts,currentAccount,setCurrentAccount,balance, receiverUsername, setReceiverUsername, transferAmount, setTransferAmount}:TransferFormData) => {
 
-    const handlerTransfer=(e: React.SyntheticEvent ) => {
+const TransferForm = ({
+                          accounts,
+                          setAccounts,
+                          currentAccount,
+                          balance
+                      }: TransferFormData) => {
+    const [receiverUsername, setReceiverUsername] = useState<string>("")
+    const [transferAmount, setTransferAmount] = useState<string>("")
+
+    const handlerTransfer=(e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const receiverAccount= getCurrentAccount(accounts, receiverUsername);
 
-        if(receiverAccount &&
-            currentAccount &&
-            receiverAccount.username !== currentAccount.username &&
-            +transferAmount > 0 &&
-            +transferAmount <= balance
-        ){}
+        if (!currentAccount) return null
 
-    }
+        const amount = Number(transferAmount);
+        const receiverAccount = getCurrentAccount(accounts, receiverUsername);
+        const today = new Date().toISOString();
+
+        if(
+            !receiverAccount ||
+            receiverAccount.username === currentAccount.username ||
+            amount <= 0 ||
+            amount > balance
+        ){
+            return null
+        }
+            setAccounts(prev => prev.map(acc => {
+                if (acc.username === currentAccount.username) {
+                    return {
+                        ...acc,
+                        movements: [...acc.movements, -amount],
+                        movementsDates: [...acc.movementsDates, today],
+                    };
+                }
+                if (acc.username === receiverAccount.username) {
+                    return {
+                        ...acc,
+                        movements: [...acc.movements, +amount],
+                        movementsDates: [...acc.movementsDates, today],
+                    };
+                }
+                return acc;
+            }));
+
+            setReceiverUsername('');
+            setTransferAmount('');
+        }
     return (
         <OperationCard title='transfer money' variant='transfer'>
             <form
