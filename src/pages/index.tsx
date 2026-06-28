3336640d4e442610ceacf3a649997bb5e9a9d778
omidsdgi <omid69sdgi@gmail.com>
2026-06-28 21:56:00 +0330

    fix: initialize accounts with usernames in state refactor: move receiverUsername and transferAmount state to TransferForm.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,31 @@ import Balance from "@/components/sections/Balance";
 import {useState} from "react";
 import {Account} from "@/types/Type";
 import {initialAccounts} from "@/data/mockData";
+import {createUsernames} from "@/utility/accounts/CreateUsernames";
 
 
 export default function Home() {
-    const [accounts, setAccounts] = useState<Account[]>(initialAccounts);
-    const[currentAccount, setCurrentAccount]=useState<Account|null>(null)
+    const [accounts, setAccounts] = useState<Account[]>(createUsernames(initialAccounts));
+    const[currentUsername, setCurrentUsername]=useState<string |null>(null)
     const [isSorted, setIsSorted] = useState<boolean>(false)
-    const [receiverUsername, setReceiverUsername] = useState<string>("")
-    const [transferAmount, setTransferAmount] = useState<string>("")
+    const accountWithUsernames =accounts
+    const currentAccount=accountWithUsernames.find(
+        acc =>acc.username === currentUsername
+    ) ?? null;
+
     const balance=currentAccount?.movements.reduce((acc,mov)=> acc + mov ,0) ?? 0
 
-    const displayMovements= isSorted
+    const displayMovements=isSorted
         ? currentAccount?.movements.slice().sort((a,b)=>b-a)
         : currentAccount?.movements
 
     return (
         <>
-            <Header accounts={accounts} currentAccount={currentAccount} setCurrentAccount={setCurrentAccount}/>
+            <Header
+                accounts={accountWithUsernames}
+                currentAccount={currentAccount}
+                setCurrentUsername={setCurrentUsername}
+            />
             <main className= "max-w-5xl mx-auto mt-12 opacity-100 ">
                 {currentAccount &&(
                     <>
@@ -27,15 +35,11 @@ export default function Home() {
                         <div className=' grid grid-cols-1  sm:grid-cols-[4fr_3fr] gap-6 mt-8'>
                             <MovementList move={displayMovements} account={currentAccount} />
                             <OperationPanel
-                                accounts={accounts}
+                                accounts={accountWithUsernames}
                                 setAccounts={setAccounts}
                                 currentAccount={currentAccount}
-                                setCurrentAccount={setCurrentAccount}
                                 balance={balance}
-                                receiverUsername={receiverUsername}
-                                setReceiverUsername={setReceiverUsername}
-                                transferAmount={transferAmount}
-                                setTransferAmount={setTransferAmount}/>
+                          />
                         </div>
                         <Summary isSorted={isSorted} setIsSorted={setIsSorted}  account={currentAccount} />
                     </>
