dbd8350786085d8b0cf84230451b1321f628ecce
omidsdgi <omid69sdgi@gmail.com>
2026-07-04 23:05:11 +0330

    feat: add initia logout timer logic

--- src/components/sections/Summary.tsx
+++ src/components/sections/Summary.tsx
@@ -1,10 +1,19 @@
 import {SummaryProps} from "@/types/Type";
 import {accountSummaryUtils} from "@/utility/AccountSummaryUtils";
+import {useEffect, useState} from "react";
+import {setInterval} from "node:timers";
 
 const Summary = ({isSorted, setIsSorted,account}:SummaryProps) => {
     const {totalIn, totalOut, interest}=accountSummaryUtils(account)
+    const [time, setTime] = useState(10);
 
-        return (
+    useEffect(() => {
+       const timeOut= setInterval(() => {
+        setTime(prevTime => prevTime - 1);
+        }, 1000)
+    return ()=> clearInterval(timeOut)
+    },[])
+    return (
         <footer className="grid grid-cols-1  sm:grid-cols-[4fr_3fr] gap-6 mt-8 text-xs md:text-sm text-gray-600 dark:text-gray-400">
             <div className="flex items-center  justify-between flex-1 ">
                 <div>
@@ -36,7 +45,7 @@ const Summary = ({isSorted, setIsSorted,account}:SummaryProps) => {
             </div>
 
             <p>
-                You will be logged out in{' '}
+                {`You will be logged out in ${time}`}
                 <span className="font-semibold text-black dark:text-white">
       05:00
     </span>
